93930899d1c96e2ddb30153734e1380ee9cef6bd
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-03-12 18:47:03 -0500

    caught up

---
+++ main.js
@@ -0,0 +1,95 @@
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const pies = [
+    {
+        name: 'Dutch Apple Pie',
+        ingredients: 'apples,sugar,butter,nutmeg,dutch people',
+        bakeTemp: 5000,
+        drinkPairing: 'Earl Grey Tea',
+        imageUrl: 'https://search.chow.com/thumbnail/800/0/www.chowstatic.com/assets/2013/09/30859_Recipeimage_620x413_dutch_apple_pie.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Vanilla',
+      },
+      {
+        name: 'Berry Pie',
+        ingredients: 'berries',
+        bakeTemp: 400,
+        drinkPairing: 'wine',
+        imageUrl: 'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+        instructor: 'Zoe',
+        iceCream: 'banana',
+      },
+      {
+        name: 'Pumpkin Pie',
+        ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
+        bakeTemp: 42,
+        drinkPairing: 'egg nog',
+        imageUrl: 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+        instructor: 'Michael',
+        iceCream: 'Vanilla',
+      },
+      {
+        name: 'Shoo Fly Pie',
+        ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
+        bakeTemp: 1234,
+        drinkPairing: 'Apple Cider',
+        imageUrl: 'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+        instructor: 'Michael',
+        iceCream: 'Coffee',
+      },
+      {
+        name: 'Pecan Pie',
+        ingredients: 'Pecans, sugar, butter, flour',
+        bakeTemp: 5000,
+        drinkPairing: 'Milk',
+        imageUrl: 'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+        instructor: 'Saul',
+        iceCream: 'Vanilla',
+      },
+      {
+        name:'Key Lime Pie',
+        ingredients: 'lemons, sugar, butter, flour',
+        bakeTemp: 5000,
+        drinkPairing: 'Water',
+        imageUrl: 'https://www.williams-sonoma.com/wsimgs/rk/images/dp/recipe/201851/0020/img38l.jpg',
+        instructor: 'Saul',
+        iceCream: 'none',
+      },
+    ];
+
+const pieBuilder = () => {
+    let domString = '';
+    pies.forEach((pie) => {
+        domString += `<div class="card">`;
+        domString += `<h3>${pie.name}</h3>`;
+        domString += `<p>Ingredients: ${pie.ingredients}</p>`;
+        //domString += `<p>Bake Temperature: ${pie.bakeTemp}</p>`;
+        //domString += `<p>Drink pairing: ${pie.drinkPairing}</p>`;
+        domString += `<p><img src=${pie.imageUrl}></p>`;
+        //domString += `<p>Instructor: ${pie.instructor}</p>`;
+        //domString += `<p>Ice Cream Pairing: ${pie.iceCreamPairing}</p>`;
+        domString += `</div>`;
+    })
+    printToDom('pieList', domString);
+};
+
+const buttonClick = (e) => {
+  console.log('You clicked a button!', e.target.id);
+};
+
+const buttonEvents = () => {
+  document.getElementById('Zoe').addEventListener('click', buttonClick);
+  document.getElementById('Saul').addEventListener('click', buttonClick);
+  document.getElementById('Michael').addEventListener('click', buttonClick);
+  document.getElementById('All').addEventListener('click', buttonClick);
+};
+
+const init = () => {  
+  buttonEvents();
+  pieBuilder();
+};
+
+init();
